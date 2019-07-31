a9c61fc2e61654678fe7d4aa00ba747794adac54
Macintosh <tuguscript@gmail.com>
2019-07-30 18:42:01 -0700

    1

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,22 +1,25 @@
 import React from "react"
 import { Helmet } from "react-helmet"
 import { graphql } from "gatsby"
+import Layout from "../components/layout"
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query we'll write in a bit
 }) {
   const { markdownRemark: post } = data // data.markdownRemark holds our post data
   return (
-    <div className="blog-post-container">
-      <Helmet title={`Your Blog Name - ${post.frontmatter.title}`} />
-      <div className="blog-post">
-        <h1>{post.frontmatter.title}</h1>
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: post.html }}
-        />
+    <Layout>
+      <div className="blog-post-container">
+        <Helmet title={`Your Blog Name - ${post.frontmatter.title}`} />
+        <div className="blog-post">
+          <h1>{post.frontmatter.title}</h1>
+          <div
+            className="blog-post-content"
+            dangerouslySetInnerHTML={{ __html: post.html }}
+          />
+        </div>
       </div>
-    </div>
+    </Layout>
   )
 }
 
